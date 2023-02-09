ada964a9bd75bde51bc26caa9a62d66511884600
SimenBU <123465276+SimenBU@users.noreply.github.com>
2023-02-09 17:40:08 +0100

    Oppdater main.ts, main.py

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ Temp = "Temperatur"
 Temp_Verdi = 0
 
 def ReadSensor():
-    return pins.analog_read_pin(AnalogPin.P1)
+    return input.temperature()
 
 def on_forever():
     Temp_Verdi
-    Temp_Verdi = ReadSensor()*4
+    Temp_Verdi = ReadSensor()
     serial.write_value(Temp, Temp_Verdi)
-    basic.pause(30000)
+    basic.pause(1000)
 basic.forever(on_forever)
