59ab428cbf5477fbd9a965a7cb350023077c5488
SimenBU <123465276+SimenBU@users.noreply.github.com>
2023-02-09 17:36:39 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,7 @@ def ReadSensor():
     return pins.analog_read_pin(AnalogPin.P1)
 
 def on_forever():
-    Temp_Verdi
-    Temp_Verdi = ReadSensor()*4
+    Temp_Verdi = ReadSensor()
     serial.write_value(Temp, Temp_Verdi)
-    basic.pause(30000)
+    basic.pause(1000)
 basic.forever(on_forever)
